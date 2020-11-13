5a59d3580554e410f7943c6ddb7fd1b0747c5690
Matija Frandolić <matija.frandolic@fer.hr>
2020-11-13 18:32:26 +0100

    Dodan kontoler za igrača

---
+++ backend/src/main/java/hr/fer/progi/ferllowship/geofighter/controller/PlayerController.java
@@ -0,0 +1,47 @@
+package hr.fer.progi.ferllowship.geofighter.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import hr.fer.progi.ferllowship.geofighter.dao.PlayerRepository;
+import hr.fer.progi.ferllowship.geofighter.model.Admin;
+import hr.fer.progi.ferllowship.geofighter.model.Cartograph;
+import hr.fer.progi.ferllowship.geofighter.model.Player;
+
+@RestController
+public class PlayerController {
+	
+	@Autowired
+	private PlayerRepository playerRepository;
+	
+	@GetMapping(path = "/player")
+	public Map<String, String> getPlayer(@RequestParam("username") String username) {
+		Map<String, String> response = new HashMap<>();
+		
+		Player player = playerRepository.findByUsername(username);
+		if (player == null) {
+			response.put("error", "Igrač ne postoji.");
+			return response;
+		}
+		
+		response.put("username", player.getUsername());
+		response.put("email", player.getEmail());
+		response.put("photoLink", player.getPhotoLink());
+		
+		if (player instanceof Admin) {
+			response.put("authorityLevel", "admin");
+		} else if (player instanceof Cartograph) {
+			response.put("authorityLevel", "cartograph");
+		} else {
+			response.put("authorityLevel", "player");
+		}
+
+        return response;
+	}
+
+}
