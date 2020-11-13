fc5bef10b05bcdbdf8c89c605d376d396230b18d
Matija Frandolić <matija.frandolic@fer.hr>
2020-11-13 18:07:53 +0100

    Promijenjen način autorizacije

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ import '../styles/App.css'
 import '../styles/Header.css'
 import {Redirect, Route} from "react-router-dom";
 
-import SubmitButton from './SubmitButton';
+import { toast } from 'react-toastify';
 
 class Header extends React.Component {
 
@@ -16,32 +16,46 @@ class Header extends React.Component {
         }
     }
 
-    doLogout() {
-        this.setState({
-            redirect: true
-        });
-        localStorage.removeItem('username');
-        localStorage.removeItem('password');
-        localStorage.removeItem('isLoggedIn');
+    async doLogout() {    
+        try {
+            let res = await fetch('/api/logout', {
+                method: 'post'
+            });
+            
+            if (res.ok) {
+                this.state.redirect = true;
+                localStorage.removeItem('username');
+                localStorage.removeItem('password');
+                localStorage.removeItem('isLoggedIn');
+                window.location.reload();
+            } else {
+                toast("Dogodila se pogreška.")
+            }
+        } catch (e) {
+            toast("Dogodila se pogreška.")
+        }
     }
 
     render() {
-        return (
-            <header className="absolute w-full bg-white background-color">
-                <div className="flex justify-between items-center border-b p-3">
-                    <span className="logo-title-light">
-                        GeoFighter
-                    </span>
-                    <div className="flex justify-between items-center">
-                        <div className="logoutButton">
-                            <button class="btnLogout" onClick={() => this.doLogout()}>Odjavi se</button> 
+        if (this.state.redirect) {
+            return (<Redirect push to="/"/>);
+        } else {
+            return (
+                <header className="absolute w-full bg-white background-color">
+                    <div className="flex justify-between items-center border-b p-3">
+                        <span className="logo-title-light">
+                            GeoFighter
+                        </span>
+                        <div className="flex justify-between items-center">
+                            <div className="logoutButton">
+                                <button class="btnLogout" onClick={() => this.doLogout()}>Odjavi se</button> 
+                            </div>
+                            <Navigation />
                         </div>
-                        <Navigation />
                     </div>
-                </div>
-                { this.state.redirect ? (<Redirect push to="/"/>) : null }
-            </header>
-        );
+                </header>
+            );
+        }
     }
 }
 
