ebe6ee3ecd35e8f9dd8d36b0a0195acb11f7ffb4
Palma Kopic <palma.kopic@fer.hr>
2020-12-16 23:14:45 +0100

    Implementacije mape

---
+++ frontend/src/components/MapComponent.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../styles/MapStyle.css";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+class MapComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLocation: { lat: 45.815399, lng: 15.966568 },
+      zoom: 12,
+    };
+  }
+  render() {
+    const { currentLocation, zoom } = this.state;
+    return (
+      <>
+        <Header />
+        <div className="mapContainer">
+        <MapContainer center={currentLocation} zoom={zoom} className="map">
+          <TileLayer
+            url="https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=JiIiuxyafWjR1SPu3uIu"
+            attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+          />
+          <Marker position={currentLocation}></Marker>
+        </MapContainer>
+        </div>
+      </>
+    );
+  }
+}
+
+export default MapComponent;
--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -1,47 +1,19 @@
-import React, { Component } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Component } from "react";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
-import "../styles/MapPage.css";
-import "leaflet/dist/leaflet.css";
-import Leaflet from "leaflet";
-
-import icon from "leaflet/dist/images/marker-icon.png";
-import iconShadow from "leaflet/dist/images/marker-shadow.png";
-import AddLocation from '../components/AddLocation'
-
-let DefaultIcon = Leaflet.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow,
-});
+import MapComponent from "../components/MapComponent";
 
 
-Leaflet.Marker.prototype.options.icon = DefaultIcon;
 
 class MapPage extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     Locations: [],
-  //     currentLocation: { lat: 45.815399, lng: 15.966568 },
-  //     zoom: 12,
-  //   };
-  // }
-
-
-   render() {
-  //   const { currentLocation, zoom } = this.state;
-
+   
+  render(){
     return (
       <>
-        <Header />
-        <div id="mapid">
-          
-        </div>
-        
-        <Footer />
+      <Header/>
+      <MapComponent/>
       </>
-    );
+      
+    )
   }
 }
 
