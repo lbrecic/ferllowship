ee2e9def5131b36f16410c1d10bb6d6997c94cad
Palma Kopic <palma.kopic@fer.hr>
2020-12-21 23:31:29 +0100

    Početak rada na osobnoj statistici

---
+++ frontend/src/components/PersonalStats.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class PersionalStats extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          fightList: [],
+          fightsWon: null,
+          fightsLost: null,
+        };
+      }
+}
