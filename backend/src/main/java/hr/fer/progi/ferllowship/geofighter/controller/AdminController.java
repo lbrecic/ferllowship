93e8d94772e14d7c08666d353ef76d125e402393
Nikola Petrovic <nikola.petrovic@fer.hr>
2021-01-10 19:45:38 +0100

    Omoguceno promoviranje igraca od strane admina

--- backend/src/main/java/hr/fer/progi/ferllowship/geofighter/controller/AdminController.java
+++ backend/src/main/java/hr/fer/progi/ferllowship/geofighter/controller/AdminController.java
@@ -1,22 +1,35 @@
 package hr.fer.progi.ferllowship.geofighter.controller;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import hr.fer.progi.ferllowship.geofighter.dao.AdminRepository;
 import hr.fer.progi.ferllowship.geofighter.dao.BanRepository;
+import hr.fer.progi.ferllowship.geofighter.dao.CartographRepository;
+import hr.fer.progi.ferllowship.geofighter.dao.LocationRepository;
 import hr.fer.progi.ferllowship.geofighter.dao.PlayerRepository;
+import hr.fer.progi.ferllowship.geofighter.dto.LocationDTO;
+import hr.fer.progi.ferllowship.geofighter.dto.MessageDTO;
 import hr.fer.progi.ferllowship.geofighter.dto.PlayerDTO;
+import hr.fer.progi.ferllowship.geofighter.model.Admin;
 import hr.fer.progi.ferllowship.geofighter.model.Ban;
+import hr.fer.progi.ferllowship.geofighter.model.Cartograph;
+import hr.fer.progi.ferllowship.geofighter.model.Location;
 import hr.fer.progi.ferllowship.geofighter.model.Player;
+import hr.fer.progi.ferllowship.geofighter.service.CloudinaryService;
 import hr.fer.progi.ferllowship.geofighter.service.PlayerService;
 
 @RestController
@@ -30,7 +43,19 @@ public class AdminController {
 
 	@Autowired
 	private BanRepository banRepository;
+	
+	@Autowired
+	private CartographRepository cartographRepository;
+	
+	@Autowired
+	private AdminRepository adminRepository;
+	
+//	@Autowired
+//	private LocationRepository locationRepository;
 
+	@Autowired
+	private CloudinaryService cloudinaryService;
+	
 	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping(path = "/allPlayers")
 	public List<PlayerDTO> getAllPlayers() {
@@ -92,5 +117,73 @@ public class AdminController {
 			banRepository.save(ban);
 		}
 	}
-
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@PostMapping(path = "/player/promote")
+	public MessageDTO promotePlayerToAdmin(@RequestParam String email) {
+		Player player = playerRepository.findByEmail(email);
+		String iban = "";
+		String idPhotoLink = "";
+		if (player instanceof Admin) {
+			return new MessageDTO("Player is already an admin!");
+		} else if (player instanceof Cartograph) {
+			iban = ((Cartograph) player).getIban();
+			idPhotoLink = ((Cartograph) player).getIdPhotoLink();
+		}
+		playerService.changeRoleToAdmin(player, iban, idPhotoLink);
+		
+		return new MessageDTO("Player's role successfully changed.");
+	}
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@PostMapping(path = "/player/changeRole")
+	public MessageDTO changeRoleOfPlayer(@RequestPart String email,
+								@RequestPart String newAuthorityLevel,
+								@RequestPart String iban,
+								@RequestPart MultipartFile picture,
+								@RequestPart String newRole
+								/*@RequestParam("email") String email,
+								@RequestParam("iban") String iban,
+								@RequestParam("idPhotoLink") String idPhotoLink,
+								@RequestParam("newRole") String newRole*/) throws IOException {
+		Player player = playerRepository.findByEmail(email);
+		
+		String idPhotoLink = "";
+		if (!Arrays.equals(picture.getBytes(),  new byte[0])) {
+			idPhotoLink = cloudinaryService.upload(picture.getBytes());
+		}
+		
+		switch(newRole) {
+		case "player":
+			if (player instanceof Cartograph || player instanceof Admin) {
+				// request pri testiranju vraca internal server error,
+				// ali uloga korisnika se uspjesno u bazi promijeni u igraca
+				playerService.changeRoleToPlayer(player);
+			} else {
+				return new MessageDTO("User is already just a player!");
+			}
+		case "cartograph":
+			if (player instanceof Cartograph) {
+				return new MessageDTO("Player is already a cartographer!");
+			} else if (player instanceof Admin) {
+				iban = ((Admin) player).getIban();
+				idPhotoLink = ((Admin) player).getIdPhotoLink();
+			}
+			if (iban.isBlank() || idPhotoLink.isBlank()) {
+				return new MessageDTO("iban and idPhotoLink needed to promote player to cartographer!");
+			}
+			playerService.changeRoleToCartograph(player, iban, idPhotoLink, true);
+			break;
+		case "admin":
+			if (player instanceof Admin) {
+				return new MessageDTO("Player is already an admin!");
+			} 
+			playerService.changeRoleToAdmin(player, iban, idPhotoLink);
+			break;
+		default:
+			return new MessageDTO("Role was incorrectly entered!");
+		}
+		return new MessageDTO("Player's role successfully changed!");
+	}
+		
 }
