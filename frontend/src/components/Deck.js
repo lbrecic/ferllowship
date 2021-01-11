61ac67b012a79bc4c8addff9feff9f9d48b48ea7
Palma Kopic <palma.kopic@fer.hr>
2021-01-11 22:16:02 +0100

    Soft-kodirane karte u deku

--- frontend/src/components/Deck.js
+++ frontend/src/components/Deck.js
@@ -7,46 +7,43 @@ class Deck extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
+      cards: [],
     };
   }
+
+  async componentDidMount() {
+    try {
+      let res = await fetch(
+        "/api/player/deck?username=" + localStorage.username
+      );
+      let result = await res.json();
+
+      if (result) {
+        this.setState({
+          cards: result,
+
+        });
+      }
+    } catch (e) {}
+  }
+
+
 
   render() {
     return (
       <>
         <div className="deck">
           <CardGroup>
-            <div className="">
+            {this.state.cards.map((card) => (
+              <div className="">
               <DeckCard
-                locationName="Belisce 1"
-                locationPhoto="https://www.tzbelisce.net/wp-content/uploads/2014/03/GAT-600x449.jpg"
-                cardPoints="215"
+                    locationPhoto={card.location.locationPhoto}
+                    cardPoints={card.cardPoints}
+                    locationName={card.location.locationName}
+                    locationDesc={card.location.locationDesc}
               />
-            </div>
-
-            <div className="">
-              <DeckCard
-                locationName="Belisce 2"
-                locationPhoto="https://radio-belisce.net/wp-content/uploads/elementor/thumbs/zelene-o7m2gybrxykds4auvobjs4t5xruidr8022639ta3jk.jpg"
-                cardPoints="415"
-              />
-            </div>
-
-            <div className="">
-              <DeckCard
-                locationName="Belisce 3"
-                locationPhoto="https://lokalni.vecernji.hr/media/daguerre/3d/c6/33958519e178b534d305.png"
-                cardPoints="135"
-              />
-            </div>
-
-            <div className="">
-              <DeckCard
-                locationName="Belisce 4"
-                locationPhoto="https://static.gradonacelnik.hr/uploads/2019/10/01-R-SOL-E-GradskaUprava-Small-1.jpg"
-                cardPoints="187"
-              />
-            </div>
+             </div>
+            ))}            
           </CardGroup>
         </div>
       </>
