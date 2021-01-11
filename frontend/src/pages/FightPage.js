fdf9205f47307529a9ee097cc3b9b5dc8f950d04
Palma Kopic <palma.kopic@fer.hr>
2021-01-11 22:02:49 +0100

    Popravljen css za borbu

--- frontend/src/pages/FightPage.js
+++ frontend/src/pages/FightPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/FightPage.css";
+import "../styles/ChooseCards.css";
 import { withRouter } from "react-router-dom";
 import DeckCard from "../components/Card";
 import OpponentCard from "../components/OpponentCard";
@@ -29,21 +30,23 @@ class FightPage extends React.Component {
       opponentCard: [],
       currentRound: 0,
       usedCards: [],
-      winnerCard : [],
-      loserCard : [],
+      winnerCard: [],
+      loserCard: [],
     };
     //this.chooseCard = this.chooseCard.bind(this);
   }
 
   async componentDidMount() {
     try {
-      let res = await fetch("/api/player/deck?username="+localStorage.username);
+      let res = await fetch(
+        "/api/player/deck?username=" + localStorage.username
+      );
       let result = await res.json();
 
       if (result) {
         allCards = result;
         this.setState({
-          allCards: result,         
+          allCards: result,
           opponentCard: allCards[2],
           winnerCard: allCards[1],
         });
@@ -77,11 +80,11 @@ class FightPage extends React.Component {
     }
   }
 
-  confirmSelection(){
-      this.setState({
-          chosen : this.state.chosen + 1,
-      })
-      ++chosen;
+  confirmSelection() {
+    this.setState({
+      chosen: this.state.chosen + 1,
+    });
+    ++chosen;
   }
 
   chooseCard(card) {
@@ -114,18 +117,23 @@ class FightPage extends React.Component {
         usedCards: this.state.usedCards.concat(card),
       });
 
-      if(card.cardPoints > this.state.opponentCard.cardPoints){
+      if (card.cardPoints > this.state.opponentCard.cardPoints) {
         this.setState({
-          winnerCard : card,
-          loserCard : this.state.opponentCard,
+          winnerCard: card,
+          loserCard: this.state.opponentCard,
           userPoints: this.state.userPoints + 1,
         });
-      }else{
+      } else if (card.cardPoints < this.state.opponentCard.cardPoints) {
         this.setState({
-          winnerCard : this.state.opponentCard,
-          loserCard : card,
+          winnerCard: this.state.opponentCard,
+          loserCard: card,
           opponentPoints: this.state.opponentPoints + 1,
-        })
+        });
+      } else {
+        this.setState({
+          winnerCard: this.state.opponentCard,
+          loserCard: card,
+        });
       }
       this.setRoundResults();
       // var string =
@@ -136,7 +144,7 @@ class FightPage extends React.Component {
     }
   }
 
-  setRoundResults(){
+  setRoundResults() {
     this.setState({
       show: true,
     });
@@ -167,16 +175,21 @@ class FightPage extends React.Component {
                 ))}
               </div>
               {this.state.chosen === 3 && (
-                <button className="readyBtn" onClick={() => {
-                  this.confirmSelection();
+                <button
+                  className="readyBtn"
+                  onClick={() => {
+                    this.confirmSelection();
 
-                  // let fightMessage = {
-                  //   player: localStorage.username,
-                  //   opponent: this.props.fightMessages[0].player
-                  // };
-                  
-                  // this.props.stompClient.send('/app/play', {}, JSON.stringify(fightMessage));
-                }}>Ready!</button>
+                    // let fightMessage = {
+                    //   player: localStorage.username,
+                    //   opponent: this.props.fightMessages[0].player
+                    // };
+
+                    // this.props.stompClient.send('/app/play', {}, JSON.stringify(fightMessage));
+                  }}
+                >
+                  Ready!
+                </button>
               )}
             </div>
             <div className="chooseCards">
@@ -208,23 +221,34 @@ class FightPage extends React.Component {
     if (this.props.fightMessages.length === 1) {
       return (
         <div className="App background-color">
-            <div className="App-header background-color">
-                <ClipLoader color={"white"} size={50}/>
-            </div>
+          <div className="App-header background-color">
+            <ClipLoader color={"white"} size={50} />
+          </div>
         </div>
       );
     }
-    
+
     return (
       <>
         <div className="fightBody geo-color">
           <div className="fightTitle">
             <span>Fight</span>
           </div>
-          <div className="fightCards">
-            <div className="userCardsWrapper">
+          <div className="fightCards geo-color">
+            <div className="opponentCardsWrapper">
+              <div className="opponentCards">
+                <OpponentCard />
+                <OpponentCard />
+                <OpponentCard />
+              </div>
+              <div className="opponentPoints">
+                {" "}
+                <span>{this.state.opponentPoints} Opponent's cards </span>
+              </div>
+            </div>
+            <div className="userCardsWrapper geo-color">
               <span>Your cards {this.state.userPoints}</span>
-              <div className="userCards">
+              <div className="userCards geo-color">
                 {this.state.cards.map((card) => (
                   <div onClick={() => this.chooseInFight(card)}>
                     <DeckCard
@@ -236,14 +260,6 @@ class FightPage extends React.Component {
                   </div>
                 ))}
               </div>
-            </div>
-            <div className="opponentCardsWrapper">
-              <div className="opponentCards">
-                <OpponentCard />
-                <OpponentCard />
-                <OpponentCard />
-              </div>
-              <span>{this.state.opponentPoints} Opponent's cards </span>
             </div>
           </div>
           <RoundModal
