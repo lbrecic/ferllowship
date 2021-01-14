989ad700a81848848ffc238881e792ec8f8724bd
Luka Brečić <luka.brecic@fer.hr>
2021-01-14 19:20:34 +0100

    Dovrseni JUnit testovi.

--- backend/src/test/java/hr/fer/progi/ferllowship/geofighter/PlayerServiceIntegrationTest.java
+++ backend/src/test/java/hr/fer/progi/ferllowship/geofighter/PlayerServiceIntegrationTest.java
@@ -53,4 +53,9 @@ public class PlayerServiceIntegrationTest {
 
         assertEquals("admin", playerService.getAuthorityLevelOfPlayer(admin));
     }
+
+    @Test
+    public void distanceTest() {
+        assertEquals(5503.5539, playerService.distance(30, 30, 60, 90), 0.001);
+    }
 }
